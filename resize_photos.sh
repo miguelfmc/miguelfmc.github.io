966169a93dcb2312c6f598eb2ae04bfafbedd89f
miguelfmc <miguelfm95@gmail.com>
2024-01-18 13:57:09 -0800

    Changed single quotes to double quotes (mistake on purpose)

--- resize_photos.sh
+++ resize_photos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name "*.jpg" -exec sh -c 'sips -Z 640 "$1" -o "${1%.*}_resized.jpg"' _ {} \;
+find . -type f -name "*.jpg" -exec sh -c "sips -Z 640 "$1" -o "${1%.*}_resized.jpg"" _ {} \;
